836f87c5178bf02aa1ac86a64bb263e3a2d65055
MrDean <Paul---@users.noreply.github.com>
2019-06-29 16:29:37 -0600

    pushing product for grade submission

---
+++ data.js
@@ -0,0 +1,38 @@
+let data= [
+  {
+Q1: `How Big is Texas really?`,
+Option1:`268, 597 mi² give or take a few square miles`,
+Option2: `Not That Big`,
+Option3: `Kinda Big`,
+Option4: `No one Knows`
+  },
+{
+  Q2: `When did Texas become a state ?`,
+  Option1: `December 29, 1845`,
+  Option2: `July 4th 1776`,
+  Option3: `It hasn't become a state officially yet`,
+  Option4: `When Abraham Lincoln was elected as president`
+},
+{
+  Q3: `Can Texas really leave the Union legally?`,
+  Option1: `Nope, but some of them still keep talking about it.`,
+  Option2: `Yes.Everybody know that`,
+  Option3: `Yes, but only if California does first`,
+  Option4: `Only if they had a unanimous vote in the state legislature.`
+},
+{
+  Q4: `Are there lots of cowboys in Texas?`,
+  Option1: `Yes siree!`,
+  Option2: `Not even one`,
+  Option3: `They are few and far between...`,
+  Option4: `People say they exist, but no one really knows...`
+},
+{
+  Q5: `Is Tex Mex really all that  Good?`,
+  Option1: `Absolutely!`,
+  Option2: `Absolutely not!`,
+  Option3: `As long as you don't add the Queso...`,
+  Option4: `There's no such thing as Tex Mex`
+}
+
+]
--- htmlVars.js
+++ htmlVars.js
@@ -1,49 +1,51 @@
-let questions = (
+
+let questionsHTML = (
   `<section class="question-section replaceable">
+  <div class="container">
     <form role="form" action="#" id='quiz-form'>
-      <h2 class='question'>
-        How Big is Texas Really?
+      <h2 class='question'>${data[0].Q1}
         </h2>
       <fieldset>
         <label class="answer-option">
-          <input  required type="radio" name="answer" value='Not So Big' class="question-btn">
+          <input  required type="radio" name="answer" value='${data[0].Option1}' class="input1 question-btn btn1">
             <span class="answerOptionText">
-              Not So Big
+            ${data[0].Option1}
             </span>
           </label>
         </fieldset>
         <fieldset>
           <label class="answer-option">
-            <input required type="radio" name="answer" value='Pretty Big' class="question-btn">
+            <input required type="radio" name="answer" value='${data[0].Option2}' class="question-btn btn2">
               <span class="answerOptionText">
-                Pretty Big
+                ${data[0].Option2}
             </span>
           </label>
         </fieldset>
           <fieldset>
             <label class="answer-option">
-              <input required type="radio" name="answer" value='Really Big' class="question-btn">
+              <input required type="radio" name="answer" value='${data[0].Option3}' class="question-btn btn3">
                 <span class="answerOptionText">
-                  Really Big
+                  ${data[0].Option3}
             </span>
           </label>
         </fieldset>
             <fieldset>
               <label class="answer-option">
-                <input required type="radio" name="answer" value='Not So Big' class="question-btn">
+                <input required type="radio" name="answer" value='${data[0].Option4}' class="question-btn btn4">
                   <span class="answerOptionText">
-                    Smaller Than California
+                    ${data[0].Option4}
             </span>
           </label>
         </fieldset>
               <button type='submit' class="submitBtn">Submit</button>
       </form>
+      </div>
     </section>`
 );
 
 let correctIncorrect = (`
   <form role="form" action="#" class="replaceable" id='quiz-form'>
-      <h2 class="feedback-h2">Your Answer Was <span>Correct</span>...</h2>
+      <h2 class="feedback-h2">Your Answer Was <span>${questionNumber + 1 === 1 ? 'Correct' : 'Wrong'}</span>...</h2>
       <button class="next-btn" type="button" role="submit" autofocus>Next Question</button>
   </form>
 `);
